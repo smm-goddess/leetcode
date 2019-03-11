72ce5b0075c366aceb8a2de5234a4649e08fc59e
liujun286 <liujun26@jd.com>
2019-03-11 17:32:28 +0800

    103

--- 103-zigzaglevelorder/zigzaglevelorder.go
+++ 103-zigzaglevelorder/zigzaglevelorder.go
@@ -1,1 +1,91 @@
-package _03_zigzaglevelorder
+package zigzaglevelorder
+
+/*
+ * @lc app=leetcode id=103 lang=golang
+ *
+ * [103] Binary Tree Zigzag Level Order Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/description/
+ *
+ * algorithms
+ * Medium (40.57%)
+ * Total Accepted:    198K
+ * Total Submissions: 488K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, return the zigzag level order traversal of its nodes'
+ * values. (ie, from left to right, then right to left for the next level and
+ * alternate between).
+ *
+ *
+ * For example:
+ * Given binary tree [3,9,20,null,null,15,7],
+ *
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ *
+ *
+ *
+ * return its zigzag level order traversal as:
+ *
+ * [
+ * ⁠ [3],
+ * ⁠ [20,9],
+ * ⁠ [15,7]
+ * ]
+ *
+ *
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func zigzagLevelOrder(root *TreeNode) [][]int {
+	if root == nil {
+		return [][]int{}
+	}
+	preSlice := []*TreeNode{root}
+	var result [][]int
+	var afterSlice []*TreeNode
+	right := true
+	for len(preSlice) > 0 {
+		var level []int
+		for i := len(preSlice) - 1; i >= 0; i-- {
+			node := preSlice[i]
+			level = append(level, node.Val)
+			if right {
+				if node.Left != nil {
+					afterSlice = append(afterSlice, node.Left)
+				}
+				if node.Right != nil {
+					afterSlice = append(afterSlice, node.Right)
+				}
+			} else {
+				if node.Right != nil {
+					afterSlice = append(afterSlice, node.Right)
+				}
+				if node.Left != nil {
+					afterSlice = append(afterSlice, node.Left)
+				}
+			}
+		}
+		right = !right
+		result = append(result, level)
+		preSlice, afterSlice = afterSlice, preSlice
+		afterSlice = afterSlice[:0]
+	}
+	return result
+}
--- 103-zigzaglevelorder/zigzaglevelorder_test.go
+++ 103-zigzaglevelorder/zigzaglevelorder_test.go
@@ -1,1 +1,52 @@
 package zigzaglevelorder
+
+import "testing"
+
+func TestLevelOrder(t *testing.T) {
+	tree := &TreeNode{
+		3,
+		&TreeNode{9, nil, nil,},
+		&TreeNode{
+			20,
+			&TreeNode{15, nil, nil},
+			&TreeNode{7, nil, nil},
+		},
+	}
+	supposed := [][]int{{3}, {20, 9}, {15, 7}}
+	level := zigzagLevelOrder(tree)
+	for i, sli := range level {
+		for j, in := range sli {
+			if supposed[i][j] != in {
+				t.Error("Error")
+				break
+			}
+		}
+	}
+	tree = &TreeNode{
+		1,
+		&TreeNode{
+			2,
+			&TreeNode{5, nil, nil},
+			&TreeNode{11, nil, nil},
+		},
+		&TreeNode{3,
+			&TreeNode{9,
+				&TreeNode{6, nil, nil},
+				&TreeNode{8, nil, nil}},
+			&TreeNode{20,
+				&TreeNode{15, nil, nil},
+				&TreeNode{7, nil, nil},
+			},
+		},
+	}
+	supposed = [][]int{{1}, {3, 2}, {5, 11, 9, 20}, {7, 15, 8, 6}}
+	level = zigzagLevelOrder(tree)
+	for i, sli := range level {
+		for j, in := range sli {
+			if supposed[i][j] != in {
+				t.Error("Error")
+				break
+			}
+		}
+	}
+}
