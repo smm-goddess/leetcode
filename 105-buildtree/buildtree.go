bc77410193fa139908a6af7ed5fb3d5d5674c48a
liujun286 <liujun26@jd.com>
2019-03-11 18:17:32 +0800

    105

--- 105-buildtree/buildtree.go
+++ 105-buildtree/buildtree.go
@@ -1,1 +1,67 @@
 package _05_buildtree
+
+/*
+ * @lc app=leetcode id=105 lang=golang
+ *
+ * [105] Construct Binary Tree from Preorder and Inorder Traversal
+ *
+ * https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/
+ *
+ * algorithms
+ * Medium (39.62%)
+ * Total Accepted:    203.2K
+ * Total Submissions: 512.7K
+ * Testcase Example:  '[3,9,20,15,7]\n[9,3,15,20,7]'
+ *
+ * Given preorder and inorder traversal of a tree, construct the binary tree.
+ *
+ * Note:
+ * You may assume that duplicates do not exist in the tree.
+ *
+ * For example, given
+ *
+ *
+ * preorder = [3,9,20,15,7]
+ * inorder = [9,3,15,20,7]
+ *
+ * Return the following binary tree:
+ *
+ *
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ *
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) == 0 {
+		return nil
+	} else if len(preorder) == 1 {
+		return &TreeNode{preorder[0], nil, nil}
+	}
+	rootVal := preorder[0]
+	place := 0
+	for ; place < len(inorder)-1; place++ {
+		if inorder[place] == rootVal {
+			break
+		}
+	}
+	return &TreeNode{rootVal,
+		buildTree(preorder[1:place+1], inorder[0:place]),
+		buildTree(preorder[place+1:], inorder[place+1:])}
+}
--- 105-buildtree/buildtree_test.go
+++ 105-buildtree/buildtree_test.go
@@ -1,1 +1,59 @@
 package _05_buildtree
+
+import (
+	"testing"
+)
+
+func TestBuildTree(t *testing.T) {
+	preorder := []int{3, 9, 20, 15, 7}
+	inorder := []int{9, 3, 15, 20, 7}
+	tree := &TreeNode{
+		3,
+		&TreeNode{9, nil, nil},
+		&TreeNode{20,
+			&TreeNode{15, nil, nil},
+			&TreeNode{7, nil, nil}},
+	}
+	//if !equalTree(tree, buildTree(preorder, inorder)) {
+	//	t.Error("Error")
+	//}
+
+	preorder = []int{3, 9, 8, 1, 20, 15, 7}
+	inorder = []int{8, 9, 1, 3, 15, 20, 7}
+	tree = &TreeNode{
+		3,
+		&TreeNode{
+			9,
+			&TreeNode{8, nil, nil},
+			&TreeNode{1, nil, nil},
+		},
+		&TreeNode{
+			20,
+			&TreeNode{15, nil, nil},
+			&TreeNode{7, nil, nil},
+		},
+	}
+	if !equalTree(tree, buildTree(preorder, inorder)) {
+		t.Error("Error")
+	}
+	preorder = []int{1, 2}
+	inorder = []int{1, 2}
+	tree = &TreeNode{
+		1,
+		nil,
+		&TreeNode{2, nil, nil},
+	}
+	if !equalTree(tree, buildTree(preorder, inorder)) {
+		t.Error("Error")
+	}
+}
+
+func equalTree(a *TreeNode, b *TreeNode) bool {
+	if a == nil && b == nil {
+		return true
+	} else if (a == nil && b != nil) || (a != nil && b == nil) {
+		return false
+	} else {
+		return a.Val == b.Val && equalTree(a.Right, b.Right) && equalTree(a.Left, b.Left)
+	}
+}
