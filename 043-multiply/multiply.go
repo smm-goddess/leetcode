e8561b71bbd34ca559a19389f537e32ed1bc001b
liujun286 <liujun26@jd.com>
2019-04-09 15:45:28 +0800

    43 multiply

--- 043-multiply/multiply.go
+++ 043-multiply/multiply.go
@@ -1,64 +1,87 @@
 package multiply
+
 /*
-Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
-
-Example 1:
-
-Input: num1 = "2", num2 = "3"
-Output: "6"
-Example 2:
-
-Input: num1 = "123", num2 = "456"
-Output: "56088"
-Note:
-
-1. The length of both num1 and num2 is < 110.
-2. Both num1 and num2 contain only digits 0-9.
-3. Both num1 and num2 do not contain any leading zero, except the number 0 itself.
-4. You must not use any built-in BigInteger library or convert the inputs to integer directly.
-*/
+ * @lc app=leetcode id=43 lang=golang
+ *
+ * [43] Multiply Strings
+ *
+ * https://leetcode.com/problems/multiply-strings/description/
+ *
+ * algorithms
+ * Medium (30.25%)
+ * Total Accepted:    191.4K
+ * Total Submissions: 632.8K
+ * Testcase Example:  '"2"\n"3"'
+ *
+ * Given two non-negative integers num1 and num2 represented as strings, return
+ * the product of num1 and num2, also represented as a string.
+ *
+ * Example 1:
+ *
+ *
+ * Input: num1 = "2", num2 = "3"
+ * Output: "6"
+ *
+ * Example 2:
+ *
+ *
+ * Input: num1 = "123", num2 = "456"
+ * Output: "56088"
+ *
+ *
+ * Note:
+ *
+ *
+ * The length of both num1 and num2 is < 110.
+ * Both num1 and num2 contain only digits 0-9.
+ * Both num1 and num2 do not contain any leading zero, except the number 0
+ * itself.
+ * You must not use any built-in BigInteger library or convert the inputs to
+ * integer directly.
+ *
+ *
+ */
 func multiply(num1 string, num2 string) string {
-  if len(num1) == 0 || len(num2) == 0 || num1[0] == '0' || num2[0] == '0' {
-    return "0"
-  }
-  base := make([]byte, len(num1))
-  for i := 0; i < len(num1); i++{
-    base[len(num1) - i - 1] = num1[i] - '0'
-  } 
-  multi := make([][]byte, len(num2))
-  for i := 0 ; i < len(num2); i++ {
-    zeros := len(num2) - i - 1
-    multiRow := make([]byte, zeros + len(num1))
-    num := num2[i] - '0'
-    for j := 0; j < len(num1); j++{
-      multiRow[j + zeros] = num * base[j]
-    }
-    multi[i] = multiRow
-  }
-  maxLength := 0
-  for _, mul := range multi {
-    if maxLength < len(mul) {
-      maxLength = len(mul)
-    }
-  }
-  sum, result, digit := 0, 0, 1
-  for i := 0; i< maxLength; i++ {
-    for _, v := range multi {
-      if len(v) > i {
-        sum += int(v[i])
-      }
-    }
-    result = digit * (sum % 10) + result
-    digit, sum = digit * 10, sum / 10
-  }
-  for sum > 0 {
-    result =  digit * (sum % 10) + result
-    digit, sum = digit * 10, sum / 10
-  }
-  res := make([]byte, 0)
-  for result > 0 {
-    res = append([]byte{byte(result % 10) + '0'}, res...)
-    result = result / 10
-  }
-  return string(res)
+	if len(num1) == 0 || len(num2) == 0 || num1[0] == '0' || num2[0] == '0' {
+		return "0"
+	}
+	base := make([]byte, len(num1))
+	for i := 0; i < len(num1); i++ {
+		base[len(num1)-i-1] = num1[i] - '0'
+	}
+	multi := make([][]byte, len(num2))
+	for i := 0; i < len(num2); i++ {
+		zeros := len(num2) - i - 1
+		multiRow := make([]byte, zeros+len(num1))
+		num := num2[i] - '0'
+		for j := 0; j < len(num1); j++ {
+			multiRow[j+zeros] = num * base[j]
+		}
+		multi[i] = multiRow
+	}
+	maxLength := 0
+	for _, mul := range multi {
+		if maxLength < len(mul) {
+			maxLength = len(mul)
+		}
+	}
+	var result []byte
+	sum := 0
+	for i := 0; i < maxLength; i++ {
+		for _, v := range multi {
+			if len(v) > i {
+				sum += int(v[i])
+			}
+		}
+		result = append([]byte{byte(sum % 10)}, result...)
+		sum = sum / 10
+	}
+	for sum > 0 {
+		result = append([]byte{byte(sum % 10)}, result...)
+		sum = sum / 10
+	}
+	for i := range result {
+		result[i] += '0'
+	}
+	return string(result)
 }
--- 043-multiply/multiply_test.go
+++ 043-multiply/multiply_test.go
@@ -2,9 +2,17 @@ package multiply
 
 import "testing"
 
-func TestMultiply(t *testing.T){
-  s1, s2 := “0”，“123”
-  if multiply(s1, s2) == "0" {
-    t.Log("Pass")
-  }
+func TestMultiply(t *testing.T) {
+	s1, s2 := "0", "123"
+	if multiply(s1, s2) != "0" {
+		t.Error("0 * 123 Error")
+	}
+	s1, s2 = "123", "456"
+	if multiply(s1, s2) != "56088" {
+		t.Error("123 * 456 Error")
+	}
+	s1, s2 = "3242342342", "99898213213"
+	if multiply(s1, s2) != "323904206590653764846" {
+		t.Error("big Error")
+	}
 }
