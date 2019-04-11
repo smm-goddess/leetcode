2b180837ddcca5fad2b1c95d3b600eec1e4c1571
liujun286 <liujun26@jd.com>
2019-04-11 13:48:30 +0800

    61 test

--- 061-rotateright/rotateright_test.go
+++ 061-rotateright/rotateright_test.go
@@ -25,43 +25,43 @@ func equal(a, b *ListNode) bool {
 }
 
 func TestRotateRight(t *testing.T) {
-	//l := &ListNode{1,
-	//	&ListNode{2,
-	//		&ListNode{3,
-	//			&ListNode{4,
-	//				&ListNode{5, nil,}},
-	//		},
-	//	},
-	//}
-	//rotated := rotateRight(l, 2)
-	//answer := &ListNode{4,
-	//	&ListNode{5,
-	//		&ListNode{1,
-	//			&ListNode{2,
-	//				&ListNode{3, nil,}},
-	//		},
-	//	},
-	//}
-	//if !equal(rotated, answer) {
-	//	t.Error("Rotate fail")
-	//}
-	//
-	//l = &ListNode{0,
-	//	&ListNode{1,
-	//		&ListNode{2, nil},},
-	//}
-	//rotated = rotateRight(l, 4)
-	//answer = &ListNode{2,
-	//	&ListNode{0,
-	//		&ListNode{1, nil},},
-	//}
-	//if !equal(rotated, answer) {
-	//	t.Error("Rotate 2 fail")
-	//}
-	//l = &ListNode{1, nil}
-	//if !equal(rotateRight(l, 1), &ListNode{1, nil}) {
-	//	t.Error("Rotate 3 fail")
-	//}
+	l := &ListNode{1,
+		&ListNode{2,
+			&ListNode{3,
+				&ListNode{4,
+					&ListNode{5, nil}},
+			},
+		},
+	}
+	rotated := rotateRight(l, 2)
+	answer := &ListNode{4,
+		&ListNode{5,
+			&ListNode{1,
+				&ListNode{2,
+					&ListNode{3, nil}},
+			},
+		},
+	}
+	if !equal(rotated, answer) {
+		t.Error("Rotate fail")
+	}
+
+	l = &ListNode{0,
+		&ListNode{1,
+			&ListNode{2, nil}},
+	}
+	rotated = rotateRight(l, 4)
+	answer = &ListNode{2,
+		&ListNode{0,
+			&ListNode{1, nil}},
+	}
+	if !equal(rotated, answer) {
+		t.Error("Rotate 2 fail")
+	}
+	l = &ListNode{1, nil}
+	if !equal(rotateRight(l, 1), &ListNode{1, nil}) {
+		t.Error("Rotate 3 fail")
+	}
 	l := &ListNode{1, &ListNode{2, nil}}
 	if !equal(rotateRight(l, 3), &ListNode{2, &ListNode{1, nil}}) {
 		t.Error()
