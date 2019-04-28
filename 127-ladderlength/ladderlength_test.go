30d12b1dddbaab09e0404ebfd6ae16c36596ad3a
liujun286 <liujun26@jd.com>
2019-04-28 15:51:42 +0800

    127

--- 127-ladderlength/ladderlength_test.go
+++ 127-ladderlength/ladderlength_test.go
@@ -1,6 +1,9 @@
 package ladderlength
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestLadderLength(t *testing.T) {
 	beginWord := "hit"
@@ -8,26 +11,26 @@ func TestLadderLength(t *testing.T) {
 	wordList := []string{"hot", "dot", "dog", "lot", "log", "cog"}
 	if ladderLength(beginWord, endWord, wordList) != 5 {
 		t.Error("Fail 1")
+	}
 
-		wordList = wordList[:len(wordList)-1]
-		if ladderLength(beginWord, endWord, wordList) != 0 {
-			t.Error("Fail 2")
-		}
-		wordList = []string{}
-		if ladderLength(beginWord, endWord, wordList) != 0 {
-			t.Error("Fail 3")
-		}
-		beginWord = "hot"
-		endWord = "dog"
-		wordList = []string{"hot", "cog", "dog", "tot", "hog", "hop", "pot", "dot"}
-		if ladderLength(beginWord, endWord, wordList) != 3 {
-			t.Error("Fail 4")
-		}
-		beginWord = "qa"
-		endWord = "sq"
-		wordList = []string{"si", "go", "se", "cm", "so", "ph", "mt", "db", "mb", "sb", "kr", "ln", "tm", "le", "av", "sm", "ar", "ci", "ca", "br", "ti", "ba", "to", "ra", "fa", "yo", "ow", "sn", "ya", "cr", "po", "fe", "ho", "ma", "re", "or", "rn", "au", "ur", "rh", "sr", "tc", "lt", "lo", "as", "fr", "nb", "yb", "if", "pb", "ge", "th", "pm", "rb", "sh", "co", "ga", "li", "ha", "hz", "no", "bi", "di", "hi", "qa", "pi", "os", "uh", "wm", "an", "me", "mo", "na", "la", "st", "er", "sc", "ne", "mn", "mi", "am", "ex", "pt", "io", "be", "fm", "ta", "tb", "ni", "mr", "pa", "he", "lr", "sq", "ye"}
-		if ladderLength(beginWord, endWord, wordList) != 3 {
-			t.Error("Fail 5")
-		}
+	wordList = wordList[:len(wordList)-1]
+	if ladderLength(beginWord, endWord, wordList) != 0 {
+		t.Error("Fail 2")
+	}
+	wordList = []string{}
+	if ladderLength(beginWord, endWord, wordList) != 0 {
+		t.Error("Fail 3")
+	}
+	beginWord = "hot"
+	endWord = "dog"
+	wordList = []string{"hot", "cog", "dog", "tot", "hog", "hop", "pot", "dot"}
+	if ladderLength(beginWord, endWord, wordList) != 3 {
+		t.Error("Fail 4")
+	}
+	beginWord = "qa"
+	endWord = "sq"
+	wordList = []string{"si", "go", "se", "cm", "so", "ph", "mt", "db", "mb", "sb", "kr", "ln", "tm", "le", "av", "sm", "ar", "ci", "ca", "br", "ti", "ba", "to", "ra", "fa", "yo", "ow", "sn", "ya", "cr", "po", "fe", "ho", "ma", "re", "or", "rn", "au", "ur", "rh", "sr", "tc", "lt", "lo", "as", "fr", "nb", "yb", "if", "pb", "ge", "th", "pm", "rb", "sh", "co", "ga", "li", "ha", "hz", "no", "bi", "di", "hi", "qa", "pi", "os", "uh", "wm", "an", "me", "mo", "na", "la", "st", "er", "sc", "ne", "mn", "mi", "am", "ex", "pt", "io", "be", "fm", "ta", "tb", "ni", "mr", "pa", "he", "lr", "sq", "ye"}
+	if ladderLength(beginWord, endWord, wordList) != 5 {
+		t.Error(fmt.Sprintf("Fail 5, %d", ladderLength(beginWord, endWord, wordList)))
 	}
 }
