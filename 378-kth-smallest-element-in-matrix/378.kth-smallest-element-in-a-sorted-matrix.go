f955cf55e24ee4d9ce107d6947c2ef85656cf268
liujun286 <liujun26@jd.com>
2019-06-04 16:44:43 +0800

    378-heap-sort

--- 378-kth-smallest-element-in-matrix/378.kth-smallest-element-in-a-sorted-matrix.go
+++ 378-kth-smallest-element-in-matrix/378.kth-smallest-element-in-a-sorted-matrix.go
@@ -41,22 +41,60 @@ import (
  * Note:
  * You may assume k is always valid, 1 ≤ k ≤ n2.
  */
+//func kthSmallest(matrix [][]int, k int) int {
+//	size := len(matrix)
+//	indexes := make([]int, size, size)
+//	smallestIndex, smallestValue := 0, 0
+//	for k > 0 {
+//		smallestValue = math.MaxInt32
+//		for i := 0; i < size; i++ {
+//			if indexes[i] < size {
+//				if matrix[i][indexes[i]] < smallestValue {
+//					smallestValue = matrix[i][indexes[i]]
+//					smallestIndex = i
+//				}
+//			}
+//		}
+//		indexes[smallestIndex]++
+//		k--
+//	}
+//	return smallestValue
+//}
+
 func kthSmallest(matrix [][]int, k int) int {
 	size := len(matrix)
 	indexes := make([]int, size, size)
-	smallestIndex, smallestValue := 0, 0
-	for k > 0 {
-		smallestValue = math.MaxInt32
-		for i := 0; i < size; i++ {
-			if indexes[i] < size {
-				if matrix[i][indexes[i]] < smallestValue {
-					smallestValue = matrix[i][indexes[i]]
-					smallestIndex = i
-				}
-			}
+	heap, value := make([]int, size, size), make([]int, size, size)
+	for i := range heap {
+		heap[i] = i
+		value[i] = matrix[i][0]
+	}
+	heapify(heap, value, size)
+	for k > 1 {
+		indexes[heap[0]] = indexes[heap[0]] + 1
+		if indexes[heap[0]] >= size {
+			value[0] = math.MaxInt32
+		} else {
+			value[0] = matrix[heap[0]][indexes[heap[0]]]
 		}
-		indexes[smallestIndex]++
+		heapify(heap, value, size)
 		k--
 	}
-	return smallestValue
+	return matrix[heap[0]][indexes[heap[0]]]
+}
+
+func heapify(heap, values []int, size int) {
+	var leftSon, rightSon int
+	for i := (size >> 1) - 1; i >= 0; i-- {
+		leftSon = 2*i + 1
+		rightSon = 2*i + 2
+		if leftSon < size && values[leftSon] < values[i] {
+			values[leftSon], values[i] = values[i], values[leftSon]
+			heap[leftSon], heap[i] = heap[i], heap[leftSon]
+		}
+		if rightSon < size && values[rightSon] < values[i] {
+			values[rightSon], values[i] = values[i], values[rightSon]
+			heap[rightSon], heap[i] = heap[i], heap[rightSon]
+		}
+	}
 }
